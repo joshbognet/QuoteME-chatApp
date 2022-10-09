7423834e2db66fc3fdb5973bae294ad9533e320c
Josh Bognet <jbcannon9@gmail.com>
2022-10-09 09:48:03 +0100

    second

--- app.py
+++ app.py
@@ -94,39 +94,6 @@ def logout():
 
 
 
-# @socketio.on('incoming-msg')
-# def on_message(data):
-#     """Broadcast messages"""
-
-#     msg = data["msg"]
-#     username = data["username"]
-#     room = data["room"]
-#     # Set timestamp
-#     time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
-#     send({"username": username, "msg": msg, "time_stamp": time_stamp}, room=room)
-    
-    
-
-# @socketio.on('join')
-# def on_join(data):
-#     """User joins a room"""
-
-#     username = data["username"]
-#     room = data["room"]
-#     join_room(room)
-
-#     # Broadcast that new user has joined
-#     send({"msg": username + " has joined the " + room + " room."}, room=room)
-    
-    
-# @socketio.on('leave')
-# def on_leave(data):
-#     """User leaves a room"""
-
-#     username = data['username']
-#     room = data['room']
-#     leave_room(room)
-#     send({"msg": username + " has left the room"}, room=room)
 
 
 
